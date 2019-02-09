25ec31b175077e02c6acc27679e58606234cc67e
harishrb <harish.badseshi@gmail.com>
2019-02-09 15:59:46 +0530

    train classifier

--- spam_filter.py
+++ spam_filter.py
@@ -1,5 +1,10 @@
 import os
 from collections import Counter
+from sklearn.model_selection import train_test_split
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.metrics import accuracy_score
+from sklearn.externals import joblib
+
 
 def dict():
     path = 'data/emails/'
@@ -51,3 +56,27 @@ print('Feature vector length: ', len(features))
 print('Label length: ', len(labels))
 
 
+x_train, x_test, y_train, y_test = train_test_split(features, labels, test_size = 0.2)
+
+model = MultinomialNB()
+model.fit(x_train, y_train)
+pred = model.predict(x_test)
+print(pred)
+print(accuracy_score(y_test, pred))
+
+
+while True:
+    features = []
+    message = raw_input('Message: ').split(' ')
+    if message[0] == 'exit':
+        joblib.dump(model, 'spam_filter.pkl')
+        print('Exiting... Model saved as spam_filter.pkl')
+        break
+    else:
+        for word in d:
+            features.append(message.count(word[0]))
+        result = model.predict([features])
+        if result == 0:
+            print('Not Spam!')
+        else:
+            print("Spam!")
